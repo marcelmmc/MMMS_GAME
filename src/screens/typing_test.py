10127e924165e59beb536fa5604e2927e8cffc50
marcelmmc <marcel.morancalderon@epfl.ch>
2023-12-02 16:58:33 +0000

    chore: add blocking and usage of backspace

--- src/screens/typing_test.py
+++ src/screens/typing_test.py
@@ -7,8 +7,8 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 128)
 font_size = 30
-num_word_per_sentence = 3
-num_sentences = 3
+num_word_per_sentence = 4
+num_sentences = 6
 
 GAME_FONT = pygame.font.SysFont('Helvetica', font_size)
 
@@ -18,6 +18,7 @@ class TypingTest:
         self.wrong_char = False
         self.word_dict = Dictionary.load_dictionary('english_5k')
         self.sentences = self.word_dict.form_sentence(num_word_per_sentence*num_sentences)
+        self.blocking_writting = False
 
     def run(self, events):
         for event in events:
@@ -26,20 +27,20 @@ class TypingTest:
                     # a control key (ctrl, shift, alt, cmd, etc), we ignore them
                     continue
                 
-                if self.sentences[self.crt_char] == pygame.key.name(event.key):
+                name_key = ' ' if event.key == pygame.K_SPACE else pygame.key.name(event.key)
+
+                if self.blocking_writting:
+                    self.blocking_writting = not pygame.K_BACKSPACE == event.key
+
+                elif self.sentences[self.crt_char] == name_key:
                     self.wrong_char = False
                     self.crt_char += 1
-
                     if self.crt_char == len(self.sentences):
                         exit(0)
-                    elif self.sentences[self.crt_char].isspace():
-                        # ignore spaces
-                        self.crt_char += 1
-                elif pygame.key.name(event.key) == 'space':
-                    # ignore spaces
-                    continue
+                        
                 else:
                     self.wrong_char = True
+                    self.blocking_writting = True
 
         x = font_size*3
         y = font_size
@@ -49,16 +50,14 @@ class TypingTest:
             if spaces == num_word_per_sentence:
                 spaces = 0
                 x = font_size*3
-                y += font_size*4
+                y += font_size*3
 
             if char == ' ':
-                x += font_size  # Adjust spacing based on font size
                 spaces += 1
-                continue 
 
             # rendered_character = GAME_FONT.render(test_sentence, True, colors[random.randint(0,len(colors)-1)])
-            if i == self.crt_char and self.wrong_char:
-                rendered_character = GAME_FONT.render(char, True, red)
+            if i == self.crt_char and self.blocking_writting:
+                rendered_character = GAME_FONT.render('_' if char == ' ' else char, True, red)
             elif self.crt_char > i:
                 rendered_character = GAME_FONT.render(char, True, green)
             else:
